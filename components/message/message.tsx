c2aeb455328b04ba6513c7ab69853565435cfa25
Saurav Hathi <61316762+sauravhathi@users.noreply.github.com>
2023-05-22 16:32:17 +0530

    Update message.tsx

--- components/message/message.tsx
+++ components/message/message.tsx
@@ -2,6 +2,6 @@ export const Message = ({ charCount, maxCharCount }: { charCount: number, maxCha
     <p className="t-2 mt-2">
         <span className={charCount > maxCharCount ? "text-red-500" : ""}>
             {charCount}
-        </span>} / {maxCharCount}
+        </span> / {maxCharCount}
     </p>
 )
